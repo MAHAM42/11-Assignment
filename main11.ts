34da38b1966f7736c315f6cfb8aba1479d482df3
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 11:20:12 -0700

    11-Assignment
    TypeScript 45-Assignment of Q11 with Node.js

---
+++ main11.ts
@@ -0,0 +1,15 @@
+//ASSIGNMENT NO 11:
+
+/*
+Names: Store the names of a few of your friends in a array called names. 
+Print each person’s name by accessing each element in the list, one at a time.
+*/
+
+console.log("ASSIGNMENT NO 11:");
+
+let names: string[] = ["Tanzila","Bushra","kashfeen","Hafsa","Laiba"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
